a911166793992428a8452644202e2981cd9e8c8d
wooblooming <wjwj9080@naver.com>
2024-06-13 00:28:53 +0900

    first commit

---
+++ chatbot.py
@@ -0,0 +1,65 @@
+from langchain.prompts import (
+    ChatPromptTemplate,
+    MessagesPlaceholder,
+    SystemMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+)
+from langchain.chains import LLMChain
+from langchain.memory import ConversationBufferMemory
+from langchain.chat_models import ChatOpenAI
+import streamlit as st
+import time
+
+# 내가 설정한 키값을 가져옴 ( .env) 요기서
+import dotenv
+dotenv.load_dotenv()
+
+st.title("MEDEASE CHATBOT")
+
+# Set a default model
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("What is up?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+        # LLM
+        llm = ChatOpenAI()
+
+        # 챗봇이 어떤 답변을 넣어줘야 할지 결정하는 섹션
+        llm_prompt = ChatPromptTemplate(
+            messages=[
+                SystemMessagePromptTemplate.from_template(
+                    "You are a nice chatbot having a conversation with a human."
+                ),
+                # The `variable_name` here is what must align with memory
+                MessagesPlaceholder(variable_name="chat_history"),
+                HumanMessagePromptTemplate.from_template("{question}"),
+            ]
+        )
+
+        # Notice that we `return_messages=True` to fit into the MessagesPlaceholder
+        # Notice that `"chat_history"` aligns with the MessagesPlaceholder name
+        memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+        conversation = LLMChain(llm=llm, prompt=llm_prompt, verbose=True, memory=memory)
+
+        result = conversation({"question": prompt})
+        for chunk in result['text'].split():
+            full_response += chunk + " "
+            time.sleep(0.05)
+
+            message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
